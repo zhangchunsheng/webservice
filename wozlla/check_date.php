389770cddf11bc2fddedfa43305c5922626db2c1
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-11-05 17:34:40 +0800

    add check_date

--- wozlla/check_date.php
+++ wozlla/check_date.php
@@ -2,8 +2,8 @@
 	$availableDate = "2013-12-31";
 	$curDate = date("Y-m-d", time());
 	$num = (strtotime($availableDate) - strtotime($curDate)) / 60 / 60 / 24;
-	$obj -> curDate = $curDate;
-	$obj -> availableDays = $num;
+	//$obj -> curDate = $curDate;
+	//$obj -> availableDays = $num;
 	if($num > 0) {
 		$obj -> isAvailable = "wsik557gj";
 	} else {
